f90825112e1eaf8c02ccbf27e596192df0dec17e
Hanumanthe Gowda <Hanumanthe.Gowda@euromonitor.com>
2018-11-28 15:57:52 +0530

    Add project files.

---
+++ LinqToSqlMetal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqToSqlMetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
